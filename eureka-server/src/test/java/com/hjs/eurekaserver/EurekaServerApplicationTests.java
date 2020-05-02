9d6d9557b889b70d1c591ce964427bd3825185c5
hjsranger <hjsranger@163.com>
2020-05-02 15:15:07 +0800

    Eureka服务端

---
+++ eureka-server/src/test/java/com/hjs/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjs.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
